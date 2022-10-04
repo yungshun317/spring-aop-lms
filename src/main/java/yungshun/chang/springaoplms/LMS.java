adc9bada59a94dbff3666699fd935fae562cbfb5
yungshun317 <seraph317@gmail.com>
2022-10-04 11:01:14 +0800

    [FEAT] Use Java-based container configuration for Spring

--- src/main/java/yungshun/chang/springaoplms/LMS.java
+++ src/main/java/yungshun/chang/springaoplms/LMS.java
@@ -6,6 +6,10 @@ public class LMS {
 
     public static void main(String[] args) {
 
+        // Read Spring Config Java class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LMSConfig.class);
+
+        // Close the context
+        context.close();
     }
 }
--- src/main/java/yungshun/chang/springaoplms/LMSConfig.java
+++ src/main/java/yungshun/chang/springaoplms/LMSConfig.java
@@ -1,4 +1,11 @@
 package yungshun.chang.springaoplms;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("yungshun.chang.springaoplms")
 public class LMSConfig {
 }
