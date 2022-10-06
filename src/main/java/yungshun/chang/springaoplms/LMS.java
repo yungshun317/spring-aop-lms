12535390fc90e20ef669b33507c38c08db544449
yungshun317 <seraph317@gmail.com>
2022-10-06 08:14:44 +0800

    [FEAT] Include the `aspectjtools` dependency in `pom.xml` and use the `@Before` advice

--- src/main/java/yungshun/chang/springaoplms/LMS.java
+++ src/main/java/yungshun/chang/springaoplms/LMS.java
@@ -1,6 +1,7 @@
 package yungshun.chang.springaoplms;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import yungshun.chang.springaoplms.dao.AccountDAO;
 
 public class LMS {
 
@@ -8,6 +9,18 @@ public class LMS {
 
         // Read Spring Config Java class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(LMSConfig.class);
+
+        // Get the bean from Spring container
+        AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // Call the business method
+        accountDAO.addAccount();
+
+        // Call it again
+        System.out.println("\nCall the method again!\n");
+
+        // Call the business method
+        accountDAO.addAccount();
 
         // Close the context
         context.close();
