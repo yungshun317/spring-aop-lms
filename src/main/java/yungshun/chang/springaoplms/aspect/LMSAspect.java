f01aa5e2cb1a700f7357a373430260a44379d42f
yungshun317 <seraph317@gmail.com>
2022-10-07 09:11:58 +0800

    [FEAT] Create the `forDaoPackage` pointcut declaration and apply it to multiple advices

--- src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
+++ src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
@@ -2,14 +2,28 @@ package yungshun.chang.springaoplms.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LMSAspect {
 
+    @Pointcut("execution(* yungshun.chang.springaoplms.dao.*.*(..))")
+    private void forDaoPackage() {}
+
     @Before("execution(public void addUser(..))")
     public void beforeAddUserAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addUser()");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
+    }
+
+    @Before("forDaoPackage()")
+    public void performLogging() {
+        System.out.println("\n=====>>> Performing logging");
     }
 }
