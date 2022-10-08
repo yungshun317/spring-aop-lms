ab689e4caeaf14a9e40d76a0c5c7cb7c7e73f753
yungshun317 <seraph317@gmail.com>
2022-10-08 09:35:19 +0800

    [FEAT] Combine pointcut expressions using logic operators

--- src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
+++ src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
@@ -12,17 +12,29 @@ public class LMSAspect {
     @Pointcut("execution(* yungshun.chang.springaoplms.dao.*.*(..))")
     private void forDaoPackage() {}
 
+    // Create pointcut for getter methods
+    @Pointcut("execution(* yungshun.chang.springaoplms.dao.*.get*(..))")
+    private void getter() {}
+
+    // Create pointcut for setter methods
+    @Pointcut("execution(* yungshun.chang.springaoplms.dao.*.set*(..))")
+    private void setter() {}
+
+    // Create pointcut: include package... exclude getter/setter
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {}
+
     @Before("execution(public void addUser(..))")
     public void beforeAddUserAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addUser()");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performApiAnalytics() {
         System.out.println("\n=====>>> Performing API analytics");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void performLogging() {
         System.out.println("\n=====>>> Performing logging");
     }
