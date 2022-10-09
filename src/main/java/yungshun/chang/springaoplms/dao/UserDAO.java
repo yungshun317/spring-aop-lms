da61057c7d467220a16aec3719f025126f6d7f14
yungshun317 <seraph317@gmail.com>
2022-10-09 10:25:21 +0800

    [FEAT] Add constructors to the `User` entity class and update `UserDAO`

--- src/main/java/yungshun/chang/springaoplms/dao/UserDAO.java
+++ src/main/java/yungshun/chang/springaoplms/dao/UserDAO.java
@@ -3,11 +3,31 @@ package yungshun.chang.springaoplms.dao;
 import org.springframework.stereotype.Component;
 import yungshun.chang.springaoplms.entity.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class UserDAO {
 
     private String firstName;
     private String lastName;
+
+    public List<User> findUsers() {
+
+        List<User> users = new ArrayList<>();
+
+        // Create sample users
+        User tmpUser1 = new User("Morrigan", "Aensland");
+        User tmpUser2 = new User("Kula", "Diamond");
+        User tmpUser3 = new User("Blue", "Mary");
+
+        // Add them to user list
+        users.add(tmpUser1);
+        users.add(tmpUser2);
+        users.add(tmpUser3);
+
+        return users;
+    }
 
     public void addUser(User user, boolean vipFlag) {
         System.out.println(getClass() + ": Adding a user");
--- src/main/java/yungshun/chang/springaoplms/entity/User.java
+++ src/main/java/yungshun/chang/springaoplms/entity/User.java
@@ -6,6 +6,13 @@ public class User {
 
     private String lastName;
 
+    public User() {}
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
