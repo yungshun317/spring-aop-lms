95e6eb6211ac3e693e0a57f01d1f264f590ef114
yungshun317 <seraph317@gmail.com>
2022-10-09 10:39:47 +0800

    [FEAT] Add a new advice for `@AfterReturning` on the `findUsers` method

--- src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
+++ src/main/java/yungshun/chang/springaoplms/aspect/LMSAspect.java
@@ -1,6 +1,7 @@
 package yungshun.chang.springaoplms.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -8,6 +9,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import yungshun.chang.springaoplms.entity.User;
+
+import java.util.List;
 
 @Aspect
 @Component
@@ -39,5 +42,25 @@ public class LMSAspect {
                 System.out.println("User lastName: " + user.getLastName());
             }
         }
+    }
+
+    // Add a new advice for `@AfterReturning` on the `findUsers` method
+    @AfterReturning(pointcut="execution(* yungshun.chang.springaoplms.dao.UserDAO.findUsers(..))", returning="users")
+    public void afterReturningFindUsersAdvice(JoinPoint joinPoint, List<User> users) {
+
+        // Print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\\n=====>>> Executing @AfterReturning on method: " + method);
+
+        // Print out the user list returning by the method
+        System.out.println("\n=====>>> Find users: " + users);
+        
+        // Post-process the data. Convert the user names to uppercase
+
+        System.out.println("\n=====>>> Find users: " + users);
+    }
+
+    private void convertUserNamesToUpperCase(List<User> users) {
+
     }
 }
--- src/main/java/yungshun/chang/springaoplms/entity/User.java
+++ src/main/java/yungshun/chang/springaoplms/entity/User.java
@@ -28,4 +28,9 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    @Override
+    public String toString() {
+        return "User [firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
 }
